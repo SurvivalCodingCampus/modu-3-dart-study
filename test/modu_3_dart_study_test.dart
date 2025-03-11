aa597d97ef2921130d6d6de49391c3a08065734f
Dansot4891 <cnctlim94@gmail.com>
2025-03-11 11:20:24 +0900

    sample.txt 파일 작성, fvm을 이용하여 dart version 설정

--- test/modu_3_dart_study_test.dart
+++ test/modu_3_dart_study_test.dart
@@ -1,8 +1,8 @@
-import 'package:modu_3_dart_study/modu_3_dart_study.dart';
-import 'package:test/test.dart';
+// import 'package:modu_3_dart_study/modu_3_dart_study.dart';
+// import 'package:test/test.dart';
 
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
+// void main() {
+//   test('calculate', () {
+//     expect(calculate(), 42);
+//   });
+// }
