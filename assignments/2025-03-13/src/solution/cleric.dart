b28bc149f7d19d03ec9dfff8a5574b5f6b2d8dd4
helljh <junho308917@naver.com>
2025-03-13 13:08:12 +0900

    feat: Cleric 객체 정의

---
+++ assignments/2025-03-13/src/solution/cleric.dart
@@ -0,0 +1,9 @@
+class Cleric {
+  String name; // 이름
+  int hp = 50; // 공격력 50 초기화
+  final int maxHp = 50; // 최대 공격력 50 초기화
+  int mp = 10; // 마법력 10 초기화
+  final int maxMp = 10; // 최대 마법력 10 초기화
+
+  Cleric(this.name); // 생성자. 이름 지정
+}
