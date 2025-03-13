c808f976db13805c450315d6a18eed13d49fb9b1
helljh <junho308917@naver.com>
2025-03-13 13:50:58 +0900

    feat: Cleric에 pray 메서드 추가 & 테스트 완료

--- test/2025-03-13/src/solution/cleric_test.dart
+++ test/2025-03-13/src/solution/cleric_test.dart
@@ -4,13 +4,28 @@ import '../../../../assignments/2025-03-13/src/solution/cleric.dart';
 
 void main() {
   test('Cleric - selfAid 테스트', () {
+    // selfAid 테스트
     // given(준비)
     Cleric cleric = Cleric("성직자");
 
     // when(실행)
-    cleric.selfAid();
+    cleric.selfAid(); // mp -> 5
 
     // then(검증)
     expect(cleric.mp, equals(5));
+    expect(cleric.hp, equals(50));
+  });
+
+  test('Cleric - pray 테스트', () {
+    // pray 테스트
+    // given(준비)
+    Cleric cleric = Cleric("성직자");
+
+    // when(실행)
+    cleric.selfAid(); // mp -> 5
+    cleric.pray(3); // mp -> 8 ~ 10 사이의 숫자
+
+    // then(검증)
+    expect(cleric.mp, inInclusiveRange(8, 10));
   });
 }
