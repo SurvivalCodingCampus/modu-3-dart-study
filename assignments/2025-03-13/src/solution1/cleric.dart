6a0b294c6b90d96b10768dd4416f48d06e6739a9
KangJiWon <doggodking@gmail.com>
2025-03-13 13:26:32 +0900

    fix: 25-03-13 과제1 수정

--- assignments/2025-03-13/src/solution1/cleric.dart
+++ assignments/2025-03-13/src/solution1/cleric.dart
@@ -7,8 +7,8 @@ class Cleric {
 
   // 생성자
   //hp와 mp의 초기값은 정해져있으므로 생성자는 이름만값만
-  Cleric(this.name);
+  // Cleric(this.name);
 
   // 테스트 코드를 위해 mp, hp를 추가해서 검증하려고 할 때
-  // Cleric(this.name, this.hp, this.mp);
+  Cleric(this.name, this.hp, this.mp);
 }
