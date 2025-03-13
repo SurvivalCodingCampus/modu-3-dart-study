28d04926ed88cae3c400adbba08429e57a265400
KangJiWon <doggodking@gmail.com>
2025-03-13 13:00:30 +0900

    fix: 25-03-13 과제1 성직자 테스트 문구 수정

--- test/2025-03-13/cleric_test.dart
+++ test/2025-03-13/cleric_test.dart
@@ -4,7 +4,7 @@ import '../../assignments/2025-03-13/src/solution1/cleric.dart';
 
 void main() {
   // 과제1 테스트
-  test('성직자 공격 테스트', () {
+  test('성직자 준비 테스트', () {
     // 성직자 준비
     Cleric me = Cleric('강지원');
     // Cleric me = Cleric('강지원', 50, 10);
@@ -15,7 +15,5 @@ void main() {
     // mp값 검증
     expect(me.mp, inInclusiveRange(0, me.maxMp));
   });
-
-
 
 }
