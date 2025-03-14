ff61e13e81f8e4d63c182a232b79c8d242ab3442
JaeHwan <wpghks56@naver.com>
2025-03-14 15:02:37 +0900

    [Fix] const 추가에 맞게 생성자 코드 변경

--- assignments/2025-03-14/cleric.dart
+++ assignments/2025-03-14/cleric.dart
@@ -7,8 +7,5 @@ class Cleric {
 
   // instance 가 존재하기 전부터 static 은 존재하는 것을 활용해
   // 생성자에 값이 안 들어올 때 static 값 투입
-  Cleric(this.name, {this.hp, this.mp}) {
-    hp = hp ?? Cleric.maxHp;
-    mp = mp ?? Cleric.maxMp;
-  }
+  Cleric(this.name, {this.hp = maxHp, this.mp = maxMp});
 }
