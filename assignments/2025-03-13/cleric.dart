f9f1da3d05433b09b2609433117e309ce87a1a77
Dansot4891 <cnctlim94@gmail.com>
2025-03-14 10:12:59 +0900

    [chore] 250313 TIL 및 코드 정리 1. camel case 예시 추가 2. cleric class 코드 정리

--- assignments/2025-03-13/cleric.dart
+++ assignments/2025-03-13/cleric.dart
@@ -6,17 +6,14 @@ class Cleric {
   final int maxMp = 10;
   int hp = 50;
   int mp = 10;
-  
 
-  Cleric(
-    this.name,
-  );
+  Cleric(this.name);
 
   // 체력 회복 스킬
   // 체력을 최대로 회복
-  void selfAid(){
+  void selfAid() {
     // 마나 부족시 return
-    if(mp < 5){
+    if (mp < 5) {
       print("MP가 부족하여 스킬을 사용할 수 없습니다.");
       return;
     }
@@ -27,11 +24,11 @@ class Cleric {
   // 마나 회복 스킬
   // 회복량 - 캐스팅 시간 + 랜덤(0~2)
   // 반환값 - 회복량
-  int pray(int casting){
+  int pray(int casting) {
     int recoveredMp = casting + Random().nextInt(3);
     mp += recoveredMp;
     // 최대 mp를 넘어서면 최대 mp로 고정
-    if(mp > maxMp){
+    if (mp > maxMp) {
       recoveredMp += maxMp - mp;
       mp = maxMp;
     }
