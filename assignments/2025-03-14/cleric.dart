e0f3d9329b4071bf7550bf21649f8f2cc7eba159
JaeHwan <wpghks56@naver.com>
2025-03-14 14:54:44 +0900

    [Fix] const 추가

--- assignments/2025-03-14/cleric.dart
+++ assignments/2025-03-14/cleric.dart
@@ -2,8 +2,8 @@ class Cleric {
   final String name;
   int? hp;
   int? mp;
-  static int maxHp = 50;
-  static int maxMp = 10;
+  static const int maxHp = 50;
+  static const int maxMp = 10;
 
   // instance 가 존재하기 전부터 static 은 존재하는 것을 활용해
   // 생성자에 값이 안 들어올 때 static 값 투입
