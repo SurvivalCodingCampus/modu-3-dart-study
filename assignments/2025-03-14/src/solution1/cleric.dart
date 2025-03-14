e0c6ecec5f179a5beff6bc6c9cc06c821b1ea71d
KangJiWon <doggodking@gmail.com>
2025-03-14 14:53:38 +0900

    feat: 25-03-14 과제 풀이

---
+++ assignments/2025-03-14/src/solution1/cleric.dart
@@ -0,0 +1,55 @@
+import 'dart:math';
+
+class Cleric {
+  String name;
+  int hp = maxHp;
+  int mp = maxMp;
+  static final int maxHp = 50;
+  static final int maxMp = 10;
+  // static const int maxHp = 50;
+  // static const int maxMp = 10;
+
+  // 생성자
+  Cleric(this.name, {this.hp = 50, this.mp = 10});
+
+  void selfAid() {
+    // 셀프 에이드 마나 소모량
+    final int selfAidMpPt = 5;
+    // 셀프 에이드 액션
+    if (mp < selfAidMpPt) {
+      // 마나 체크
+      print('$name(은)는 셀프 에이드를 시전했지만 마나가 부족해 시전할 수 없었다.');
+    } else if (hp <= 0) {
+      // 체력 체크
+      print('$name(은)는 셀프 에이드를 시전했지만 유령이기 때문에 아무일도 일어나지 않았다.');
+    } else {
+      // 마나/체력 조건을 만족할 때
+      final int prevHp = hp;
+      final int prevMp = mp;
+
+      mp -= selfAidMpPt;
+      hp == maxHp;
+      print('$name(은)는 셀프 에이드를 시전하였다! (Hp $prevHp -> $hp / Mp:$prevMp -> $mp)');
+    }
+  }
+
+  int pray(int sec) {
+    // 기도하기(pray) 액션
+
+    if (hp <= 0) {
+      // 체력 체크
+      print('$name(은)는 기도했지만, 이미 신의 곁에 있어 아무런 일도 일어나지 않았다.');
+    }
+
+    //회복 예정인 mp값
+    int addMp = sec + Random().nextInt(2);
+
+    //최종 회복될 mp값
+    int resultAddMp = min(addMp, maxMp - mp);
+
+    mp += resultAddMp;
+    print('$name(은)는 기도하기를 $sec초 시전하여 MP $resultAddMp만큼 회복하였다. (현재 MP : $mp)');
+
+    return resultAddMp;
+  }
+}
---
+++ test/2025-03-14/cleric_test.dart
@@ -0,0 +1,74 @@
+
+import 'package:test/test.dart';
+
+import '../../assignments/2025-03-14/src/solution1/cleric.dart';
+
+void main() {
+  // 과제1-1 테스트
+
+  test('성직자 최대 HP, MP 테스트', () {
+
+    // 성직자 준비
+    Cleric woo = Cleric('우서', hp: 50, mp: 10);
+    Cleric ahn = Cleric('안두인', hp: 40, mp: 10);
+
+    //error : hp 값 최대값 초과
+    // Cleric ahn = Cleric('안두인', hp: 100, mp: 10);
+
+    //error : mp 값 최대값 초과
+    // Cleric woo = Cleric('안두인', hp: 50, mp: 99);
+
+    // 우서 최대 hp,mp값 범위 검증
+    expect(woo.hp, inInclusiveRange(0, Cleric.maxHp));
+    expect(woo.mp, inInclusiveRange(0, Cleric.maxMp));
+
+    // 안두인 최대 hp,mp값 범위 검증
+    expect(ahn.hp, inInclusiveRange(0, Cleric.maxHp));
+    expect(ahn.mp, inInclusiveRange(0, Cleric.maxMp));
+  });
+
+
+
+  // 과제1-2 테스트
+
+  test('생성자 검증 A: 전체', () {
+
+    Cleric cA = Cleric('성직자A', hp: 50, mp: 10);
+
+    // 우서 최대 hp,mp값 범위 검증
+    expect(cA.hp, inInclusiveRange(0, Cleric.maxHp));
+    expect(cA.mp, inInclusiveRange(0, Cleric.maxMp));
+
+  });
+
+  test('생성자 검증 B: mp 생략 (mp = 초기값)', () {
+
+    Cleric cB = Cleric('성직자B', hp: 50);
+
+    // 우서 최대 hp,mp값 범위 검증
+    expect(cB.hp, inInclusiveRange(0, Cleric.maxHp));
+    expect(cB.mp, inInclusiveRange(0, Cleric.maxMp));
+
+  });
+
+  test('생성자 검증 C: hp 생략 (hp = 초기값)', () {
+
+    Cleric cC = Cleric('성직자C', mp: 10);
+
+    // 우서 최대 hp,mp값 범위 검증
+    expect(cC.hp, inInclusiveRange(0, Cleric.maxHp));
+    expect(cC.mp, inInclusiveRange(0, Cleric.maxMp));
+
+  });
+
+  test('생성자 검증 D: hp, mp 생략 (hp,mp = 초기값)', () {
+
+    Cleric cD = Cleric('성직자D');
+
+    // 우서 최대 hp,mp값 범위 검증
+    expect(cD.hp, inInclusiveRange(0, Cleric.maxHp));
+    expect(cD.mp, inInclusiveRange(0, Cleric.maxMp));
+
+  });
+
+}
