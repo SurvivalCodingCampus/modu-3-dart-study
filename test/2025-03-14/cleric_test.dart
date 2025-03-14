63c247727f868713798007e5fa5907ade19e7312
yunsuk990 <yunsuk990@naver.com>
2025-03-14 16:05:07 +0900

    test: Cleric 테스트

---
+++ test/2025-03-14/cleric_test.dart
@@ -0,0 +1,41 @@
+import 'package:modu_3_dart_study/2025-03-14/Cleric.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Cleric - 이름,HP,MP를 지정', () {
+    // GIVEN
+    final Cleric cleric = Cleric('최윤석', hp: 30, mp: 5);
+
+    // THEN
+    expect(cleric.hp, equals(30));
+    expect(cleric.mp, equals(5));
+  });
+
+  test('Cleric - 이름, HP만 지정', () {
+    // GIVEN
+    final Cleric cleric = Cleric('최윤석', hp: 10);
+
+    // THEN
+    expect(cleric.hp, equals(10));
+    expect(cleric.mp, equals(10));
+  });
+
+  test('Cleric - 이름만 지정', () {
+    // GIVEN
+    final Cleric cleric = Cleric('최윤석');
+
+    // THEN
+    expect(cleric.hp, equals(50));
+    expect(cleric.mp, equals(10));
+  });
+
+  test('Cleric - 이름만 지정', () {
+    // GIVEN
+    final Cleric cleric = Cleric('최윤석');
+
+    // THEN
+    expect(cleric.hp, equals(50));
+    expect(cleric.mp, equals(10));
+  });
+
+}
