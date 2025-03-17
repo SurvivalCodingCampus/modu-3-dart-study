6b10c531e6158a4d7b78dc80a655046720e9f935
irmu98 <choiroom98@naver.com>
2025-03-17 18:26:29 +0900

    과제 코드 수정

--- assignments/2025-03-17/src/solution1/wand.dart
+++ assignments/2025-03-17/src/solution1/wand.dart
@@ -10,6 +10,7 @@ class Wand {
     if (value.length < 3) {
       throw Exception('에러메세지 : 지팡이의 이름은 3문자 이상이어야 합니다.');
     }
+    _name = value;
   }
 
   set power(double value) {
@@ -17,15 +18,16 @@ class Wand {
     if (value < 0.5 || value > 100.0) {
       throw Exception('에러메세지 : 지팡이의 마력은 0.5 이상 100.0 이하여야 합니다.');
     }
+    _power = value;
   }
 
   Wand({required String name, required double power})
-    : _name =
-          name.length < 3
-              ? throw Exception('에러메세지 : 지팡이의 이름은 3문자 이상이어야 합니다.')
-              : name,
-      _power =
-          power < 0.5 || power > 100.0
-              ? throw Exception('에러메세지 : 지팡이의 마력은 0.5 이상 100.0 이하여야 합니다.')
-              : power;
+      : _name =
+  name.length < 3
+      ? throw Exception('에러메세지 : 지팡이의 이름은 3문자 이상이어야 합니다.')
+      : name,
+        _power =
+        power < 0.5 || power > 100.0
+            ? throw Exception('에러메세지 : 지팡이의 마력은 0.5 이상 100.0 이하여야 합니다.')
+            : power;
 }
