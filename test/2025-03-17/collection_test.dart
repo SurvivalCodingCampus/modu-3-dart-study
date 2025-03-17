d9af80efcd2e14dd95213e9a814b1d40c22666e6
Dansot4891 <cnctlim94@gmail.com>
2025-03-17 17:41:52 +0900

    [chore] collection 테스트 코드 그룹화

--- test/2025-03-17/collection_test.dart
+++ test/2025-03-17/collection_test.dart
@@ -4,22 +4,24 @@ import 'package:test/test.dart';
 import '../../assignments/2025-03-17/person.dart';
 
 void main() {
-  test("컬렉션 값 출력", () {
-    final people = {"홍길동": 20, "한석봉": 25};
+  group("collection 통합 테스트", () {
+    test("컬렉션 값 출력", () {
+      final people = {"홍길동": 20, "한석봉": 25};
 
-    for (final person in people.entries) {
-      print("${person.key}의 나이는 ${person.value}살");
-    }
-  });
+      for (final person in people.entries) {
+        print("${person.key}의 나이는 ${person.value}살");
+      }
+    });
 
-  test("컬렉션과 클래스 값 출력", () {
-    final gildong = Person(name: "홍길동", year: 2005);
-    final sukbong = Person(name: "한석봉", year: 2000);
+    test("컬렉션과 클래스 값 출력", () {
+      final gildong = Person(name: "홍길동", year: 2005);
+      final sukbong = Person(name: "한석봉", year: 2000);
 
-    final List<Person> people = [gildong, sukbong];
+      final List<Person> people = [gildong, sukbong];
 
-    for (final person in people) {
-      print(person.name);
-    }
+      for (final person in people) {
+        print(person.name);
+      }
+    });
   });
 }
