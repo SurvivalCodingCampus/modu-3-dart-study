05fb10afaa9ccd7c6c4b8cb9a6f2dd0146798a1e
HyunSoo.Kim <gomtinge326@gmail.com>
2025-03-17 23:29:39 +0900

    feat : assignments완료

--- lib/PR2025-03-17/wizard.dart
+++ lib/PR2025-03-17/wizard.dart
@@ -14,6 +14,7 @@ class Wizard {
 
   Wand? get wand => _wand;
 
+  //setter로 이름 유효성 추가
   set name(String value) {
     if (value.length < 3) {
       throw Exception("이름이 너무 짧습니다.");
@@ -21,6 +22,7 @@ class Wizard {
     _name = value;
   }
 
+  //setter로 hp 유효성 추가
   set hp(int value) {
     if (value < 0) {
       _hp = 0;
@@ -30,6 +32,7 @@ class Wizard {
     }
   }
 
+  //setter로 mp 유효성 추가
   set mp(int value) {
     if (value < 0) {
       _mp = 0;
@@ -38,6 +41,7 @@ class Wizard {
     _mp = value;
   }
 
+  //setter로 wand가 추가되었을때 변경이 되지 않도록 유효성 추가
   set wand(Wand? value) {
     value == null
         ? throw Exception('지팡이가 생성된 이후에는 Null로 지정할 수 없습니다')
