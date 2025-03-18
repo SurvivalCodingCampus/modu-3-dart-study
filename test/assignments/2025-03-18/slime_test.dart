1da4860c93d6dd402ab71167bd95203fc0458dd8
Junsung Choi <choiexe1@gmail.com>
2025-03-18 14:29:30 +0900

    test: 2025-03-18 과제 테스트코드 작성

---
+++ test/assignments/2025-03-18/slime_test.dart
@@ -0,0 +1,48 @@
+import 'package:test/test.dart';
+
+import '../../../assignments/2025-03-18/hero.dart';
+import '../../../assignments/2025-03-18/poison_slime.dart';
+import '../../../assignments/2025-03-18/slime.dart';
+
+void main() {
+  group('[Slime]', () {
+    test('공격 메서드를 호출하면 데미지를 입혀야한다.', () {
+      // Given
+      final Slime slime = Slime('Slime 1');
+      final Hero hero = Hero('Hero 1');
+
+      // When
+      slime.attack(hero);
+
+      // Then
+      expect(hero.hp, equals(Hero.maxHp - Slime.ap));
+    });
+  });
+
+  group('[PoisonSlime]', () {
+    test('공격 메서드를 호출하면 추가로 독 데미지를 입혀야한다.', () {
+      // Given
+      final PoisonSlime poisonSlime = PoisonSlime('PoisonSlime 1');
+      final Hero hero = Hero('Hero 1');
+      final int finalHp = Hero.maxHp - Slime.ap - (Hero.maxHp - Slime.ap) ~/ 5;
+
+      // When
+      poisonSlime.attack(hero);
+
+      // Then
+      expect(hero.hp, equals(finalHp));
+    });
+    test('poisonCount가 0이면 추가 독 데미지를 입힐 수 없어야한다.', () {
+      // Given
+      final PoisonSlime poisonSlime = PoisonSlime('PoisonSlime 2');
+      final Hero hero = Hero('Hero 1');
+
+      // When
+      poisonSlime.poisonCount = 0;
+      poisonSlime.attack(hero);
+
+      // Then
+      expect(hero.hp, equals(Hero.maxHp - Slime.ap));
+    });
+  });
+}
