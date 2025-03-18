a0f1a7f09682eccdbdfb56c89fcb5f19cd691f9f
Dansot4891 <cnctlim94@gmail.com>
2025-03-18 16:20:15 +0900

    [feat, refactor] wizard의 최대mp값을 final로 선언하여 상속받는게 가능하도록 설정

--- assignments/2025-03-18/wizard.dart
+++ assignments/2025-03-18/wizard.dart
@@ -3,10 +3,16 @@ import 'dart:math';
 import '../2025-03-18/hero.dart';
 
 class Wizard {
-  static const maxMp = 100; // 최대 Mp
+  final maxMp = 100; // 최대 Mp
   final int _usedMp = 10; // 소모 마나
   final int _healedHp = 20; // 힐량
-  int mp = maxMp;
+  // late 선언
+  // -> 생성자에서 무조건 할당이 되도록 선언
+  late int mp;
+
+  Wizard(){
+    mp = maxMp;
+  }
 
   void heal(Hero hero) {
     if (mp < _usedMp) {
@@ -20,7 +26,8 @@ class Wizard {
 }
 
 class GreatWizard extends Wizard {
-  static const maxMp = 150; // 최대 마나
+  @override
+  int get maxMp => 150; // 최대 마나
   @override
   int get _usedMp => 50; // 소모 마나
   @override
