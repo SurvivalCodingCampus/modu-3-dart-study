189d23d70d963d8759225d3e833374234c789982
Junsung Choi <choiexe1@gmail.com>
2025-03-18 14:37:46 +0900

    remove: unnecessary class

--- assignments/2025-03-18/wizard.dart
+++ assignments/2025-03-18/wizard.dart
@@ -1,20 +1,7 @@
 import 'hero.dart';
-import 'wand.dart';
 
 class Wizard extends Hero {
-  Wand? _wand;
-
   Wizard(super.name);
-
-  set wand(Wand? wand) {
-    if (wand == null) {
-      throw Exception('완드는 null으로 수정할 수 없습니다.');
-    }
-
-    _wand = wand;
-  }
-
-  Wand? get wand => _wand;
 
   void heal(Hero target) {
     const int mpCost = 10;
