7b1ebbc45a4521c442c07386efbb4eab41a29431
eastar-DS <sien0104@gmail.com>
2025-03-19 12:29:09 +0900

    lecture: 추상화클래스, 인터페이스

---
+++ lib/2025-03-19/interface.dart
@@ -0,0 +1,51 @@
+import 'package:modu_3_dart_study/2025-03-18/inheritance.dart';
+
+abstract interface class Human {
+  void speak();
+}
+
+abstract interface class Attackable {
+  void attack(Slime slime);
+}
+
+
+
+class Book extends TangibleAsset {
+  String isbn;
+
+  Book(super.name, super.price, super.color, super.weight, this.isbn);
+}
+
+class Computer extends TangibleAsset {
+  String makerName;
+
+  Computer(super.name, super.price, super.color, super.weight, this.makerName);
+}
+
+abstract interface class Thing {
+  // property : 메소드의 일종
+  double get weight;
+  set weight(double value);
+}
+
+abstract class Asset {
+  String name;
+  int price;
+  double _weight;
+
+  Asset(this.name, this.price, double weight) : 
+    _weight = weight;
+}
+
+abstract class TangibleAsset extends Asset implements Thing {
+  String color;
+  TangibleAsset(super.name, super.price, super.weight, this.color);
+
+  @override
+  double get weight => _weight;
+
+  @override
+  set weight(double value) {
+    _weight = value;
+  }
+}
