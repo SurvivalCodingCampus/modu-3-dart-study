5694daaf2083c2abfd76ecb065264fd4fe5b344b
complete0415jiyoung <complete0415@naver.com>
2025-03-19 15:28:41 +0900

    FIX: 20250318 수정사항 커밋

--- lib/2025-03-14/slime.dart
+++ lib/2025-03-14/slime.dart
@@ -1,6 +1,13 @@
-class Slime {
-  final int level = 10;
-  int hp;
+import 'package:modu_3_dart_study/2025-03-14/hero.dart';
 
-  Slime({required this.hp});
+class Slime {
+  int hp = 50;
+  final String suffix;
+
+  Slime(this.suffix);
+
+  void attack(Hero hero) {
+    print('슬라임 $suffix이/가 공격했다');
+    hero.receiveDamage(10);
+  }
 }
