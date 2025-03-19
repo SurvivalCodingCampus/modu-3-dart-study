0710793bb0c214dcffccac11fc73def44674559d
eastar-DS <sien0104@gmail.com>
2025-03-19 13:09:22 +0900

    feat: 연습문제 코드 작성

---
+++ assignments/2025-03-19/book.dart
@@ -0,0 +1,7 @@
+import 'tangible_asset.dart';
+
+class Book extends TangibleAsset {
+  String isbn;
+
+  Book(super.name, super.price, super.color, super.weight, this.isbn);
+}
---
+++ assignments/2025-03-19/thing.dart
@@ -0,0 +1,5 @@
+abstract interface class Thing {
+  // property : 메소드의 일종
+  double get weight;
+  set weight(double value);
+}
