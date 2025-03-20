066c5be5442e3e2aec947c5cfdeda7b8e892709b
yunsuk990 <yunsuk990@naver.com>
2025-03-20 23:07:25 +0900

    feat: 2025-03-20 과제

---
+++ test/2025-03-20/a_test.dart
@@ -0,0 +1,16 @@
+import 'package:modu_3_dart_study/2025-03-20/%EC%97%B0%EC%8A%B5%EB%AC%B8%EC%A0%9C%202/a.dart';
+import 'package:modu_3_dart_study/2025-03-20/%EC%97%B0%EC%8A%B5%EB%AC%B8%EC%A0%9C%202/b.dart';
+import 'package:modu_3_dart_study/2025-03-20/%EC%97%B0%EC%8A%B5%EB%AC%B8%EC%A0%9C%202/x.dart';
+import 'package:modu_3_dart_study/2025-03-20/%EC%97%B0%EC%8A%B5%EB%AC%B8%EC%A0%9C%202/y.dart';
+import 'package:test/scaffolding.dart';
+
+void main(){
+  test("연습문제2", (){
+
+    A a = A();
+    B b = B();
+
+
+
+  });
+}
