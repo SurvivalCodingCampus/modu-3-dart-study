01b37e53a8cdd00bdd0765833ff3e3500ab2bf02
JaeHwan <wpghks56@naver.com>
2025-03-21 16:41:27 +0900

    [Feature] 03/21 assignment create

---
+++ assignments/2025-03-21/book.dart
@@ -0,0 +1,29 @@
+class Book {
+  final String title;
+  final DateTime _publishDate;
+  final String comment;
+
+  Book({required this.title, required this.comment, DateTime? publishDate})
+    : _publishDate = publishDate ?? DateTime.now();
+
+  DateTime get publishDate =>
+      DateTime(_publishDate.year, _publishDate.month, _publishDate.day);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Book &&
+          runtimeType == other.runtimeType &&
+          publishDate == other.publishDate;
+
+  @override
+  int get hashCode => publishDate.hashCode;
+
+  Book copyWith({String? title, DateTime? publishDate, String? comment}) {
+    return Book(
+      title: title ?? this.title,
+      publishDate: publishDate ?? _publishDate,
+      comment: comment ?? this.comment,
+    );
+  }
+}
