43299afe579ce4aeef67c7f3541bb6cee3d02113
Dansot4891 <cnctlim94@gmail.com>
2025-03-21 17:23:42 +0900

    [feat] 과제 수행 1. book class 구현 2. 각 요구사항에 맞게 함수 추가 및 기능 수정 3. 테스트 코드 작성

---
+++ test/2025-03-21/book_test.dart
@@ -0,0 +1,44 @@
+import 'package:test/test.dart';
+
+import '../../assignments/2025-03-21/book.dart';
+
+void main() {
+  group('book test', () {
+    final books = [
+      Book(title: "운수좋은날", comment: "재밌음"),
+      Book(title: "홍길동전", comment: "재밌음", publishDate: DateTime(2020)),
+      Book(title: "운수좋은날", comment: "재밌음", publishDate: DateTime(2023)),
+      Book(title: "운수좋은날", comment: "재밌음"),
+      Book(title: "운수좋은날", comment: "재밌음", publishDate: DateTime(2025, 3, 20)),
+      Book(title: "운수좋은날", comment: "재밌음", publishDate: DateTime(2025, 3, 22)),
+      Book(title: "가시고기", comment: "재밌음", publishDate: DateTime(2021)),
+    ];
+    test('book == 함수 테스트', () {
+      expect(books[0] == books[3], true);
+      expect(books[0] == books[4], false);
+      expect(books[0] == books[5], false);
+    });
+
+    test('book sort 함수 테스트', () {
+      print("-- sort before --");
+      for (final book in books) {
+        print(book.toString());
+      }
+      books.sort();
+      print("-- sort after --");
+      for (final book in books) {
+        print(book.toString());
+      }
+    });
+
+    test('book copywith 함수 테스트', () {
+      final bookTitle = "책책책";
+      final bookComment = "내용내용내용";
+      final book1 = books[0].copywith(title: bookTitle, comment: bookComment);
+
+      expect(book1.title, bookTitle);
+      expect(book1.comment, bookComment);
+      expect(book1.publishDate, books[0].publishDate);
+    });
+  });
+}
