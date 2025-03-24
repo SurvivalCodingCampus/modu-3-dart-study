36948e74a4391cbe63b7f3875cf3505343497064
Si Yujun <siuj0810@gmail.com>
2025-03-24 09:34:30 +0900

    fix: 2025-03-21 test 코드 추가

---
+++ test/2025_03_21/solution1_test.dart
@@ -0,0 +1,27 @@
+import 'package:test/test.dart';
+
+import '../../assignments/2025_03_21/book.dart';
+
+void main() {
+  Book book1 = Book(title: '개발', comment: '인스턴스');
+  Book book2 = Book(title: '개발', comment: '추상클래스');
+  group('Book', () {
+    test('제목과 출간일이 같으면 같은 책이다.', () {
+      expect(book1 == book2, isTrue);
+    });
+    test('신상 출간일 순서대로 정렬', () {
+      List bookList = <Book>[];
+
+      bookList.add(book1);
+      bookList.add(book2);
+
+      print(bookList);
+    });
+    test('copyWith() 메서드', () {
+      Book book3 = book1.copyWith();
+
+      expect(book1 == book3, isFalse);
+      print(book3);
+    });
+  });
+}
