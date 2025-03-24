dca0e490f7b7beed6e98f5453d4295b723fd80e1
KangJiWon <doggodking@gmail.com>
2025-03-24 15:51:53 +0900

    feat : 2025-03-24 연습문제 풀이 (코드만)

---
+++ lib/2025-03-24/strong_box.dart
@@ -0,0 +1,31 @@
+enum KeyType {
+  padlock(1024),
+  button(10000),
+  dial(30000),
+  finger(1000000);
+
+  final int maxAttempts;
+
+  const KeyType(this.maxAttempts);
+}
+
+class StrongBox<T> {
+  T? _item;
+  KeyType key;
+  int attempts = 0; // 사용 카운트
+
+  StrongBox(this.key);
+
+  void put(T item) {
+    _item = item;
+  }
+
+  T? get() {
+    if (attempts < key.maxAttempts) {
+      attempts++;
+      return _item;
+    } else {
+      return null;
+    }
+  }
+}
---
+++ lib/2025-03-24/ward.dart
@@ -0,0 +1,29 @@
+class Word {
+  String word = '';
+
+  bool isVowel(int i) {
+    const String vowels = "aeiou";
+
+    // 입력된 인덱스와 문자열의 길이 유효성 검사
+    if (i < 0 || i >= word.length) {
+      throw RangeError("유효하지 않는 인덱스: $i. 입력할 수 있는 값은 (0-${word.length - 1}) 사이");
+    }
+
+
+    String char = word[i];
+
+    // 알파벳 유효성 검사 (정규표현식에 사용하는 RegExp 함수를 이용)
+    // https://api.dart.dev/stable/dart-core/RegExp-class.html
+    if (!RegExp(r'^[a-zA-Z]$').hasMatch(char)) {
+      throw ArgumentError("이 문자열의 $i index 알파벳이 아니기 때문에 자음 여부를 검사할 수 없음");
+    }
+    // 모음인지 확인
+    return vowels.contains(char.toLowerCase());
+  }
+
+  bool isConsonant(int i) {
+    // 자음은 모음이 아니고, 알파벳이어야 함
+    return !isVowel(i);
+  }
+}
+
