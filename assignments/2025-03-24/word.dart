8010542cea1d8694338f86fd10b5aa93933f0873
eastar-DS <sien0104@gmail.com>
2025-03-24 15:21:44 +0900

    feat: word클래스, 테스트코드 작성

---
+++ assignments/2025-03-24/word.dart
@@ -0,0 +1,29 @@
+class Word {
+  String word = '';
+
+  Word(this.word);
+
+  bool isVowel(int i) {
+    if (i >= word.length) {
+      print('word의 길이보다 큰 수를 넣을 수 없습니다.');
+      return false;
+    }
+    const List<String> vowels = ['a', 'e', 'i', 'u', 'o'];
+    for (final vowel in vowels) {
+      if (word[i] == vowel || word[i] == vowel.toUpperCase()) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  bool isConsonant(int i) {
+    if (i >= word.length) {
+      print('word의 길이보다 큰 수를 넣을 수 없습니다.');
+      return false;
+    } else {
+      return !isVowel(i);
+    }
+  }
+}
