64edfd2b9ce084ca50366bbb2ba525ff0b44b21e
eastar-DS <sien0104@gmail.com>
2025-03-24 17:50:29 +0900

    fix: 과제 피드백 후 코두 수정

--- assignments/2025-03-24/strong_box.dart
+++ assignments/2025-03-24/strong_box.dart
@@ -1,24 +1,34 @@
-enum KeyType { padlock, button, dial, finger }
+enum KeyType {
+  padlock(lock: 1024),
+  button(lock: 10000),
+  dial(lock: 30000),
+  finger(lock: 1000000);
+
+  const KeyType({required this.lock});
+  final int lock;
+}
 
 class StrongBox<T> {
   T? _instance;
-  final Enum _keyType;
+  final KeyType _keyType;
   late int _attempts;
 
-  StrongBox({T? instance, required Enum keyType})
+  StrongBox({T? instance, required KeyType keyType})
     : _instance = instance,
-      _keyType = keyType {
-    switch (_keyType) {
-      case KeyType.padlock:
-        _attempts = 1024;
-      case KeyType.button:
-        _attempts = 10000;
-      case KeyType.dial:
-        _attempts = 30000;
-      case KeyType.finger:
-        _attempts = 1000000;
-    }
-  }
+      _keyType = keyType,
+      _attempts = keyType.lock;
+  //   {
+  // switch (_keyType) {
+  //   case KeyType.padlock:
+  //     _attempts = 1024;
+  //   case KeyType.button:
+  //     _attempts = 10000;
+  //   case KeyType.dial:
+  //     _attempts = 30000;
+  //   case KeyType.finger:
+  //     _attempts = 1000000;
+  // }
+  // }
 
   void put(T instance) {
     _instance = instance;
