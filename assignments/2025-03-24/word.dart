d3d9eaee57aa841341491fd0938e7774a40d7c18
eastar-DS <sien0104@gmail.com>
2025-03-24 16:11:12 +0900

    fix: Word 클래스 메소드 수정

--- assignments/2025-03-24/word.dart
+++ assignments/2025-03-24/word.dart
@@ -20,8 +20,7 @@ class Word {
 
   bool isConsonant(int i) {
     if (i >= word.length) {
-      print('word의 길이보다 큰 수를 넣을 수 없습니다.');
-      return false;
+      return isVowel(i);
     } else {
       return !isVowel(i);
     }
