bfa40a032657f51f1fb92993c7d7e42b58fb2da2
Kein Moon <successhelper2000@gmail.com>
2025-03-26 07:56:39 +0900

    feat: file operations 및 테스트 코드 작성

--- lib/2025-03-25/default_file_operations.dart
+++ lib/2025-03-25/default_file_operations.dart
@@ -2,19 +2,18 @@ import 'dart:io';
 import 'package:modu_3_dart_study/2025-03-25/file_operations.dart';
 
 class DefaultFileOperations implements FileOperations {
-
   @override
-  void copy(String sourcePath, String targetPath){
-
+  void copy(String sourcePath, String targetPath) {
     try {
       //원본 파일을 연다.
-      File sourceFile = File(sourcePath);
+      final sourceFile = File(sourcePath);
       File targetFile = sourceFile.copySync(targetPath); //파일 복사
-      print('복사 완료:${copiedFile.path}');
+      print('복사 완료:${targetFile.path}');
+    } on PathNotFoundException {
+      print('경로가 잘못 되었습니다.');
+      throw ArgumentError('경로가 잘못 되었습니다.');
     } catch (e) {
       print('파일 복사 오류 발생 : $e');
     }
-
   }
-
 }
--- test/2025-03-25/default_file_operations_test.dart
+++ test/2025-03-25/default_file_operations_test.dart
@@ -1,16 +1,26 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'dart:io';
+
 import 'package:modu_3_dart_study/2025-03-25/default_file_operations.dart';
+import 'package:test/test.dart';
 
 void main() {
+  group('[DefaultFileOperations] - ', () {
+    DefaultFileOperations fileOperations = DefaultFileOperations();
+    String sourceFileName = 'sourceFile.txt';
+    String targetFileName = 'targetFileName.txt';
 
-  DefaultFileOperations defaultFileOperations = DefaultFileOperations();
-  
+    test('파일 copy', () {
+      fileOperations.copy(sourceFileName, targetFileName);
+      expect(File(targetFileName).existsSync(), isTrue);
+    });
 
-  try {
+    test('파일명이 잘못되었을 때', () {
+      sourceFileName = 'wrongFileName.txt';
 
-    defaultFileOperations.copy(sourcePath, targetPath);
-
-  } catch (e) {
-    print('파일 복사 오류 발생 : $e');
-  }
+      expect(
+        () => fileOperations.copy(sourceFileName, targetFileName),
+        throwsA(isArgumentError),
+      );
+    });
+  });
 }
