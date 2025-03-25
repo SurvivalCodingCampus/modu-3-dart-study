5f4a258ade8fd5d286670b5c601683ccde13498d
Kein Moon <successhelper2000@gmail.com>
2025-03-26 06:35:10 +0900

    feat: exception 처리 numString 구현완료

--- lib/2025-03-25/exception.dart
+++ lib/2025-03-25/exception.dart
@@ -1,30 +1,24 @@
 void main() {
-
-  ExcptionTest1();
-  ExcptionTest2();
+  ExcptionTest1(); // catch 문으로 예외 핸들링
+  ExcptionTest2(); // FormatException 문으로 예외 핸들링
 }
 
 void ExcptionTest1() {
-
   final numString = '10.5';
   int num;
 
   try {
-
     //FormatException: Invalid radix-10 number (at character 1)
     num = int.parse(numString);
     print('바로 앞에서 터져서 출력이 안되네: $num');
-
   } catch (e) {
     num = 0;
-    print('정수가 아닙니다:\n$e');
-    print('catch문에서 num 값 $num 으로 셋팅');
-    print('-----------------------');
+    print('정수가 아닙니다. catch문에서 num 값 $num 으로 설정합니다.');
+    print(e);
   }
 }
 
 void ExcptionTest2() {
-
   final numString = '10.5';
   int num;
 
@@ -32,10 +26,8 @@ void ExcptionTest2() {
     //FormatException: Invalid radix-10 number (at character 1)
     num = int.parse(numString);
     print(num);
-
   } on FormatException {
     num = 0;
-    print("정수가 아닙니다, 기본값을 0으로 사용해야합니다.");
-    print('FormatException 문에서 num 값 $num 으로 셋팅');
+    print('정수가 아닙니다. FormatException문에서 num 값 $num 으로 설정합니다.');
   }
 }
