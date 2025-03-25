a33b0fa50c4d2582efea512bef0664d0091f14b5
eastar-DS <sien0104@gmail.com>
2025-03-25 09:48:27 +0900

    fix: static 키워드 추가

--- assignments/2025-03-24/word.dart
+++ assignments/2025-03-24/word.dart
@@ -3,13 +3,15 @@ class Word {
 
   Word(this.word);
 
+  static const List<String> vowels = ['a', 'e', 'i', 'u', 'o'];
+
   bool isVowel(int i) {
     if (i >= word.length || i < 0) {
       throw Exception(
         '해당 인덱스에 해당하는 문자가 없습니다. 0 이상, ${word.length} 미만의 숫자를 입력해주세요',
       );
     }
-    const List<String> vowels = ['a', 'e', 'i', 'u', 'o'];
+
     for (final vowel in vowels) {
       if (word[i].toLowerCase() == vowel) {
         return true;
