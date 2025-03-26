a596e9afce7942b4d198284a7fa71e0638d659ed
Si Yujun <siuj0810@gmail.com>
2025-03-26 19:29:06 +0900

    fix: 2025-03-24 test 코드 수정

--- test/2025_03_24/solution1_test.dart
+++ test/2025_03_24/solution1_test.dart
@@ -11,7 +11,7 @@ void main() {
   group('Strong Box', () {
     test('Strong Box class 정의', () {
       StrongBox strongBox = StrongBox(element: '', key: KeyType.padLock);
-      expect(strongBox, isA());
+      expect(strongBox, isA<StrongBox>());
     });
     test('KeyType 열거형 정의', () {
       StrongBox strongBox1 = StrongBox(element: '', key: KeyType.padLock);
@@ -31,12 +31,44 @@ void main() {
       expect(strongBox4.key, KeyType.finger);
       expect(strongBox4.getKeyTypeInt(), fingerKey);
     });
-    test('StrongBox get()', () {
+    test('StrongBox get() count 증가', () {
       StrongBox strongBox = StrongBox(element: '', key: KeyType.padLock);
+      int keycount = strongBox.keyTypeCount;
+      print(keycount);
 
       strongBox.get();
+      expect(keycount + 1, strongBox.keyTypeCount);
+    });
 
-      expect(strongBox.keyTypeCount, 1);
+    group('keyLock test', () {
+      test('padLock test', () {
+        StrongBox strongBox1 = StrongBox(element: '과자', key: KeyType.padLock);
+        strongBox1.keyTypeCount = 1023;
+        strongBox1.get();
+
+        expect(strongBox1.element, strongBox1.get());
+      });
+      test('buttonLock test', () {
+        StrongBox strongBox2 = StrongBox(element: '과자', key: KeyType.button);
+        strongBox2.keyTypeCount = 9999;
+        strongBox2.get();
+
+        expect(strongBox2.element, strongBox2.get());
+      });
+      test('dialLock test', () {
+        StrongBox strongBox3 = StrongBox(element: '과자', key: KeyType.dial);
+        strongBox3.keyTypeCount = 29999;
+        strongBox3.get();
+
+        expect(strongBox3.element, strongBox3.get());
+      });
+      test('fingerLock test', () {
+        StrongBox strongBox4 = StrongBox(element: '과자', key: KeyType.finger);
+        strongBox4.keyTypeCount = 999999;
+        strongBox4.get();
+
+        expect(strongBox4.element, strongBox4.get());
+      });
     });
   });
 }
--- test/2025_03_24/solution2_test.dart
+++ test/2025_03_24/solution2_test.dart
@@ -1,8 +1,39 @@
 import 'package:test/test.dart';
 
+import '../../assignments/2025_03_24/string/word.dart';
+
 void main() {
   group('문자열', () {
-    test('isVowel Test', () {});
-    test('isConsonant Test', () {});
+    final word = Word(word: 'abcdef');
+    test('isVowel Test', () {
+      word.isVowel(0); // 'a'
+      word.isVowel(1); // 'b'
+      word.isVowel(2); // 'c'
+      word.isVowel(3); // 'd'
+      word.isVowel(4); // 'e'
+      word.isVowel(5); // 'f'
+
+      expect(word.isVowel(0), isTrue);
+      expect(word.isVowel(1), isFalse);
+      expect(word.isVowel(2), isFalse);
+      expect(word.isVowel(3), isFalse);
+      expect(word.isVowel(4), isTrue);
+      expect(word.isVowel(5), isFalse);
+    });
+    test('isConsonant Test', () {
+      word.isConsonant(0); // 'a'
+      word.isConsonant(1); // 'b'
+      word.isConsonant(2); // 'c'
+      word.isConsonant(3); // 'd'
+      word.isConsonant(4); // 'e'
+      word.isConsonant(5); // 'f'
+
+      expect(word.isConsonant(0), isFalse);
+      expect(word.isConsonant(1), isTrue);
+      expect(word.isConsonant(2), isTrue);
+      expect(word.isConsonant(3), isTrue);
+      expect(word.isConsonant(4), isFalse);
+      expect(word.isConsonant(5), isTrue);
+    });
   });
 }
