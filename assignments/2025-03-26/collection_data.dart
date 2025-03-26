4ca84f99ce19bc984aae13d7fbcffc336e7166f2
complete0415jiyoung <complete0415@naver.com>
2025-03-26 21:32:56 +0900

    FEAT: 20250326 과제제출

---
+++ assignments/2025-03-26/collection_data.dart
@@ -0,0 +1,33 @@
+import 'collectio_sale_price.dart';
+
+class CollectionData {
+  String collectionName;
+  List<CollectioSalePrice>? collectionSalePrice;
+
+  CollectionData({required this.collectionName, this.collectionSalePrice});
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> result = {'collectionName': collectionName};
+
+    if (collectionSalePrice != null) {
+      result['collectionSalePrice'] =
+          collectionSalePrice?.map((e) => e.toJson()).toList();
+      // map 함수: 리스트에 있는 각요소를 가져옴,,e에 담김 각 요소를 toJson 객체-> map 형태로 만들고
+      // .toList() 사용해서 다시 리스트로 만들기..ㄷㄷ
+    } // null 일때는 담지도 않음,,,
+
+    return result;
+  }
+
+  // 역직렬화
+  CollectionData.fromJson(Map<String, dynamic> json)
+    : collectionName = json['collectionName'],
+      collectionSalePrice =
+          (json['collectionSalePrice'] as List?)
+              ?.map((e) => CollectioSalePrice.fromJson(e))
+              .toList();
+
+  @override
+  String toString() =>
+      'CollectionData(collectionName: $collectionName, collectionSalePrice: $collectionSalePrice)';
+}
