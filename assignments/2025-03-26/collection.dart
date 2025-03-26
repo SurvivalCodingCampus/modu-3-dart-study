830592dcacbf0f2b926b392710dfb6180864c00b
HyunSoo.Kim <gomtinge326@gmail.com>
2025-03-26 18:01:17 +0900

    2025-03-26 과제 완료

---
+++ assignments/2025-03-26/collection.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+import 'dart:io';
+
+class SalePrice {
+  final double price;
+  final DateTime cvtDatetime;
+
+  SalePrice(this.price, this.cvtDatetime);
+
+  SalePrice.fromJson(Map<String, dynamic> json)
+    : price = json["price"],
+      cvtDatetime = DateTime.parse(json["cvtDatetime"]);
+}
+
+class ChartData {
+  final String collectionName;
+
+  //원본에 collectionSalePrice이곳은 List 형식으로 저장되어있어 List형식으로 선언
+  final List<SalePrice> collectionSalePrice;
+
+  ChartData(this.collectionName, this.collectionSalePrice);
+
+  ChartData.fromJson(Map<String, dynamic> json)
+    : collectionName = json["collectionName"],
+      //값을 리스트 형태로 변환
+      collectionSalePrice =
+          (json["collectionSalePrice"] as List)
+              .map((item) => SalePrice.fromJson(item))
+              .toList();
+}
