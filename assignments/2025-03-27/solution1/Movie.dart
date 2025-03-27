01addfb6d891961744764137571aa0b04b36102a
Yunseo <og737596@gmail.com>
2025-03-27 18:10:51 +0900

    feat : 2025-03-27 TIL & assignments

---
+++ assignments/2025-03-27/solution1/Movie.dart
@@ -0,0 +1,42 @@
+class Movie {
+  String title;
+  String originalTitle;
+  String description;
+  List genre;
+  int year;
+  double rating;
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return 'Movie{ title : $title, originalTitle : $originalTitle, '
+        'description : $description, genre :$genre, year : $year, rating : '
+        '$rating }';
+  }
+
+  Movie({
+    required this.title,
+    required this.originalTitle,
+    required this.description,
+    required this.genre,
+    required this.year,
+    required this.rating,
+  });
+
+  Map<String, dynamic> toJson() => {
+    "title": title,
+    "originalTitle": originalTitle,
+    "genre": genre,
+    "year": year,
+    "rating": rating,
+    "description": description,
+  };
+
+  Movie.fromJson(Map<String, dynamic> json)
+    : title = json["title"],
+      originalTitle = json["original_title"],
+      description = json["description"],
+      genre = json["genre"],
+      year = json["year"],
+      rating = json["rating"];
+}
---
+++ test/2025-03-27/solution1/movies_test.dart
@@ -0,0 +1,8 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:test/test.dart';
+
+void main() {
+  test('영화 데이터 불러오기', () {});
+}
