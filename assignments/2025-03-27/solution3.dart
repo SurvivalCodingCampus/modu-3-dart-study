238a605a41f6d3ccdddc009efd490f3c4f6a61ce
complete0415jiyoung <complete0415@naver.com>
2025-03-27 19:00:21 +0900

    FEAT:20250327과제제출

---
+++ assignments/2025-03-27/solution3.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+void main() async {
+  print('🌅 아침이 밝았습니다!');
+
+  Bird bird1 = Bird(sound: '🐦‍꾸우', interval: 1);
+  Bird bird2 = Bird(sound: '🐤까악', interval: 2);
+  Bird bird3 = Bird(sound: '🐧짝짹', interval: 3);
+
+  bool flag = true;
+
+  Future.delayed(Duration(seconds: 10), () {
+    flag = false;
+    print("🪟창문닫음!");
+    exit(0); // 강제종료,,,,
+  });
+
+  await Future.wait([
+    bird1.makeSound(() => flag),
+    bird2.makeSound(() => flag),
+    bird3.makeSound(() => flag),
+  ]);
+
+  print("끝");
+}
+
+class Bird {
+  String sound;
+  int interval;
+
+  Bird({required this.sound, required this.interval});
+
+  Future<void> makeSound(bool Function() timer) async {
+    while (timer()) {
+      await Future.delayed(Duration(seconds: interval));
+      print('$sound (${DateTime.now()})');
+    }
+  }
+}
--- test/2025-03-25/copy_file_test.dart
+++ test/2025-03-25/copy_file_test.dart
@@ -5,22 +5,24 @@ import '../../assignments/2025-03-25/default_file_operations.dart';
 void main() {
   final DefaultFileOperations dfo = DefaultFileOperations();
   group('copyFile만들기', () {
-    test('정상적인 파일복사 ', () {
+    test('정상적인 파일복사 ', () async {
       final String sourcePath =
           Directory.current.path + '/lib/2025-03-25/file.dart';
-      final String targetPath =
-          Directory.current.path + '/lib/2025-03-25';
+      final String targetPath = Directory.current.path + '/lib/2025-03-25';
 
-      expect(() => dfo.copy(sourcePath, targetPath), returnsNormally);
+      expect(
+        () async => await dfo.copy(sourcePath, targetPath),
+        returnsNormally,
+      );
     });
-    test('잘못된 복사대상파일경로', () {
+    test('잘못된 복사대상파일경로', () async {
       final String wrongSourcePath =
           '/Users/jilong/Desktop/workspace/flutter_modu/lib/invalidPath/file.dart';
       final String targetPath =
           '/Users/jilong/Desktop/workspace/flutter_modu/assignments/2025-03-25';
 
       expect(
-        () => dfo.copy(wrongSourcePath, targetPath),
+        () async => await dfo.copy(wrongSourcePath, targetPath),
         throwsA(
           isA<ArgumentError>().having(
             (e) => e.message,
@@ -31,14 +33,14 @@ void main() {
       );
     });
 
-    test('잘못된 복사파일경로', () {
+    test('잘못된 복사파일경로', () async {
       final String sourcePath =
           '/Users/jilong/Desktop/workspace/flutter_modu/lib/2025-03-25/file.dart';
       final String wrongTargetPath =
           '/Users/jilong/Desktop/workspace/flutter_modu/invalidTargetPath';
 
       expect(
-        () => dfo.copy(sourcePath, wrongTargetPath),
+        () async => await dfo.copy(sourcePath, wrongTargetPath),
         throwsA(
           isA<ArgumentError>().having(
             (e) => e.message,
