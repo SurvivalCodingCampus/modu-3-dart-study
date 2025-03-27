2f80ed271b103a955b4c50622f041c04acc2e042
complete0415jiyoung <complete0415@naver.com>
2025-03-27 19:01:13 +0900

    FIX: 복습

--- assignments/2025-03-25/default_file_operations.dart
+++ assignments/2025-03-25/default_file_operations.dart
@@ -4,7 +4,7 @@ import 'file_operations.dart';
 
 class DefaultFileOperations implements FileOperations {
   @override
-  void copy(String sourcePath, String targetPath) {
+  Future<void> copy(String sourcePath, String targetPath) async {
     // sourcePath : 복사할 원본 파일의 경로
     // targetPath : 파일이 복사될 대상의 경로
     try {
@@ -22,17 +22,17 @@ class DefaultFileOperations implements FileOperations {
 
       if (lastDotIndex != -1) {
         // 점(.)이 있는 경우
-        originFileName = originFilePath.substring(0, lastDotIndex); 
-        originFileExtension = originFilePath.substring(lastDotIndex + 1); 
+        originFileName = originFilePath.substring(0, lastDotIndex);
+        originFileExtension = originFilePath.substring(lastDotIndex + 1);
       } else {
         // 점(.)이 없는 경우
-        originFileName = originFilePath; 
-        originFileExtension = '.txt'; 
+        originFileName = originFilePath;
+        originFileExtension = '.txt';
       }
 
       // print('파일명 : $originFileName, 확장자: $originFileExtension');
       // 1-2 복사 대상의 내용 읽어오기
-      final List<int> originFileContent = originFile.readAsBytesSync();
+      final List<int> originFileContent = await originFile.readAsBytesSync();
 
       // 2. 복사본 파일
       // 2-1. 복사본 파일의 이름과 주소 생성 (StringBuffer)
@@ -61,13 +61,13 @@ class DefaultFileOperations implements FileOperations {
       // 2-4. 복사 대상에 파일 넣기
       copyFile.writeAsBytesSync(originFileContent);
     } on PathNotFoundException catch (e) {
-      print('오류 메시지: ${e.message}');
-      print('문제 경로: ${e.path}');
+      // print('오류 메시지: ${e.message}');
+      // print('문제 경로: ${e.path}');
       throw ArgumentError('[ArgumentError] 파일 경로를 확인해 주세요');
     } on FileSystemException catch (e) {
       print('파일 작업 중 문제가 발생했습니다:');
-      print('오류 메시지: ${e.message}');
-      print('문제 경로: ${e.path}');
+      // print('오류 메시지: ${e.message}');
+      // print('문제 경로: ${e.path}');
       throw FileSystemException('[FileSystemException] 파일 작업 중 문제가 발생했습니다:');
     }
   }
