558aba4fe1bc95985d9fb65e91660a30829d1175
Yunseo <og737596@gmail.com>
2025-03-29 20:12:15 +0900

    fix : 테스트 코드 추가

--- lib/2025-03-27/solution2/bird_sound.dart
+++ lib/2025-03-27/solution2/bird_sound.dart
@@ -1,19 +1,14 @@
 import 'dart:async';
 
-void main() async {
-  Future bird({required String sound, required int second}) async {
-    int counter = 4;
-    Timer.periodic(Duration(seconds: second), (timer) {
-      print(sound);
-      counter--;
-      if (counter == 0) {
-        timer.cancel();
-        print("카운트 종료");
-      }
-    });
-  }
+void main() async {}
 
-  await bird(sound: "꾸우", second: 1);
-  await bird(sound: "까악", second: 2);
-  await bird(sound: "짹짹", second: 3);
+Future bird({required String sound, required int second}) async {
+  int counter = 4;
+  Timer.periodic(Duration(seconds: second), (timer) {
+    print(sound);
+    counter--;
+    if (counter == 0) {
+      timer.cancel();
+    }
+  });
 }
--- test/2025-03-27/solution1/movies_test.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:convert';
-import 'dart:io';
-
-import 'package:test/test.dart';
-
-void main() {
-  test('영화 데이터 불러오기', () {});
-}
