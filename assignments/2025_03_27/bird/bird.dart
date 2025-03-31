17ca46353a102b61edbc7c141c873f1550e0f660
Si Yujun <siuj0810@gmail.com>
2025-03-31 10:06:17 +0900

    fix: solution2 추가

--- assignments/2025_03_27/bird/bird.dart
+++ assignments/2025_03_27/bird/bird.dart
@@ -22,4 +22,42 @@ class Bird {
       print('짹짹');
     }
   }
+
+  Future<void> pigeonInfy() async {
+    while (isSound) {
+      await Future.delayed(Duration(seconds: 1));
+      if (!isSound) return;
+      print('꾸우');
+    }
+  }
+
+  Future<void> crowInfy() async {
+    while (isSound) {
+      await Future.delayed(Duration(seconds: 2));
+      if (!isSound) return;
+      print('까악');
+    }
+  }
+
+  Future<void> sparrowInfy() async {
+    while (isSound) {
+      await Future.delayed(Duration(seconds: 3));
+      if (!isSound) return;
+      print('짹짹');
+    }
+  }
+
+  Future<void> stopSound(
+    Future<void> function1,
+    Future<void> function2,
+    Future<void> function3,
+  ) async {
+    try {
+      await Future.delayed(Duration(seconds: soundCount));
+      isSound = false;
+      return;
+    } finally {
+      print('프로그램 종료');
+    }
+  }
 }
