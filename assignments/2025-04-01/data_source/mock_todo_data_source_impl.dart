983a31da42a37a1c91978050c3576d62a1e322c4
멋쟁이 개발일기 <137850782+complete0415Jiyoung@users.noreply.github.com>
2025-04-02 09:01:40 +0900

    Update mock_todo_data_source_impl.dart

--- assignments/2025-04-01/data_source/mock_todo_data_source_impl.dart
+++ assignments/2025-04-01/data_source/mock_todo_data_source_impl.dart
@@ -1,7 +1,7 @@
 // 가짜 데이터 소스 (테스트용)
 import 'data_source.dart';
 
-class MockTodoDataSourceImple implements DataSource {
+class MockTodoDataSourceImpl implements DataSource {
   @override
   Future<List<dynamic>> getList(filePath) async {
     await Future.delayed(Duration(seconds: 1)); // 네트워크 요청처럼 딜레이 추가
