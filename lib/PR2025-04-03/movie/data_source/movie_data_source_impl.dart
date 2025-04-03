4a40ea95520c1ba9abb3dd8e3df54c3b26ba3ed6
HyunSoo.Kim <gomtinge326@gmail.com>
2025-04-04 00:45:11 +0900

    feat: image과제 추가

--- lib/PR2025-04-03/movie/data_source/movie_data_source_impl.dart
+++ lib/PR2025-04-03/movie/data_source/movie_data_source_impl.dart
@@ -3,13 +3,12 @@ import 'package:http/http.dart' as http;
 import 'movie_data_source.dart';
 
 class MovieDataSourceImpl implements MovieDataSource {
+  static const url =
+      'https://api.themoviedb.org/3/movie/upcoming?api_key=a64533e7ece6c72731da47c9c8bc691f&language=ko-KR&page=1';
+
   @override
   Future<List<Map<String, dynamic>>> getUpcomingMovies() async {
-    final response = await http.get(
-      Uri.parse(
-        'https://api.themoviedb.org/3/movie/upcoming?api_key=a64533e7ece6c72731da47c9c8bc691f&language=ko-KR&page=1',
-      ),
-    );
+    final response = await http.get(Uri.parse(url));
     if ((response.statusCode == 200)) {
       //api에서 받아온 json 파일을 Map<String, dynamic> 디코딩해주기
       final Map<String, dynamic> jsonData = jsonDecode(response.body);
