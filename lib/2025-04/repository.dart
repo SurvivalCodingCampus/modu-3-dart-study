fe4b41ea7ea18233608edc7e4ee103f414d683d9
Junsung Choi <choiexe1@gmail.com>
2025-04-07 17:32:32 +0900

    feat: predicate function

--- lib/2025-04/repository.dart
+++ lib/2025-04/repository.dart
@@ -1,4 +1,5 @@
-abstract interface class Repository<T, KEY> {
+abstract interface class Repository<T, ID, DTO> {
+  Future<T?> findOne(ID id);
+  Future<T?> findByFilter(bool Function(T predicate) predicate);
   Future<List<T>> findAll();
-  Future<T> findOne(KEY id);
 }
