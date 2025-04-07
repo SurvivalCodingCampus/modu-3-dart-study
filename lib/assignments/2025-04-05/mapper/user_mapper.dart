e6e10a0a1349473d0db0111ccfc0b5799394f913
eastar-DS <sien0104@gmail.com>
2025-04-07 15:22:13 +0900

    feat: mapper 구현

---
+++ lib/assignments/2025-04-05/mapper/user_mapper.dart
@@ -0,0 +1,19 @@
+import 'package:modu_3_dart_study/assignments/2025-04-05/dto/user_dto.dart';
+import 'package:modu_3_dart_study/assignments/2025-04-05/model/user.dart';
+
+extension UserMapper on UserDto {
+  User toUser() {
+    final ymd = createdAt.split(' ').first.split('-');
+    final year = int.parse(ymd[0]);
+    final month = int.parse(ymd[1]);
+    final day = int.parse(ymd[2]);
+    final hour = int.parse(createdAt.split(' ').last.split(':').first);
+    final userCreatedAt = DateTime(year, month, day, hour);
+    return User(
+      id: id,
+      email: email,
+      password: password,
+      createdAt: userCreatedAt,
+    );
+  }
+}
